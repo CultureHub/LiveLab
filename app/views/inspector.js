d065524ae6ae919729f8ac8f4b60a07138371feb
ojack <ojackson@gmail.com>
2020-02-01 20:34:24 -0500

    most recent streams

--- app/views/inspector.js
+++ app/views/inspector.js
@@ -20,18 +20,29 @@ module.exports = inspectorComponent
 function inspectorComponent (state, emit) {
   if(state.ui.inspector.trackId !== null){
     var media = state.media.byId[state.ui.inspector.trackId]
-    var title = html`<span class="i">Track Info: ${state.peers.byId[media.peerId].nickname}-${media.name}-${media.track.kind} </span> `
-    var innerContents =  html`<div class="pa2 w-100" style="word-wrap: break-word;font-size:11px">
-      ${media.track.kind==='video' ? Video({
-        htmlProps: {
-          class: 'h4 w4'
-        },
-        index: 'inspector',
-        track: (state.ui.inspector.trackId in state.media.byId)  ? state.media.byId[state.ui.inspector.trackId].track : null,
-        id: (state.ui.inspector.trackId in state.media.byId) ?  state.media.byId[state.ui.inspector.trackId].track.id : null
-      }) : null }
+    var title = html`<span class="i">Track Info: ${state.peers.byId[media.peerId].nickname}-${media.name} </span> `
 
-      ${Object.keys(media.settings).map((setting) => html`<div class="pb1"><span class="i">${setting}: </span> ${media.settings[setting]}</div>`)}
+     // ${media.track.kind==='video' ? Video({
+     //    htmlProps: {
+     //      class: 'h4 w4'
+     //    },
+     //    index: 'inspector',
+     //    track: (state.ui.inspector.trackId in state.media.byId)  ? state.media.byId[state.ui.inspector.trackId].track : null,
+     //    id: (state.ui.inspector.trackId in state.media.byId) ?  state.media.byId[state.ui.inspector.trackId].track.id : null
+     //  }) : null }
+  var innerContents =  html`<div class="pa2 w-100" style="word-wrap: break-word;font-size:11px">
+    ${Video({
+       htmlProps: {
+         class: 'h4 w4'
+       },
+       index: 'inspector',
+       stream: state.media.byId[state.ui.inspector.streamId],
+       id: state.ui.inspector.streamId
+     })}
+      ${Object.keys(media.settings).map((kind) => html`
+        <div class="pb1"><span class="i">${kind}: </span>
+        ${Object.keys(media.settings[kind]).map((setting) => html`<div class="pb1"><span class="i">${setting}:${media.settings[kind][setting]} </span>` )}
+      </div>`)}
 
     ${ media.peerId ===  state.user.uuid ? html`<div class="f6 fr ma2 link ph3 pv2 mb2 white bg-dark-pink pointer dib dim" onclick=${() => (emit('peers:hangupTrack', state.ui.inspector.trackId))}>Hangup</div>` : null }
 
