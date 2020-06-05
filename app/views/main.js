615a774890ad6c98788c0a3bf2fb433e256a72a4
ojack <ojackson@gmail.com>
2020-06-05 15:59:22 +0200

    linter

--- app/views/main.js
+++ app/views/main.js
@@ -1,5 +1,4 @@
 'use strict'
-
 const html = require('choo/html')
 const Login = require('./login.js')
 const communication = require('./communication.js')
@@ -9,22 +8,23 @@ const onload = require('on-load')
 /* panels */
 const peersList = require('./peersList.js')
 const settingsPanel = require('./settings.js')
-const Chat = require( './chat.js')
-const Audio = require( './audio.js')
-const Switcher = require( './switcher.js')
+const Chat = require('./chat.js')
+const Audio = require('./audio.js')
+const Switcher = require('./switcher.js')
 const AddMedia = require('./addMedia.js')
 
-
 const modal = (content, name, label, state, emit) => {
-  if(state.layout.panels[name]) {
+  if (state.layout.panels[name]) {
     return html`
       <div class="pa2 pa4-ns fixed w-100 h-100 top-0 left-0" style="pointer-events:all;background:rgba(51, 51, 51, 0.8);">
-      <i
-              class="fas fa-times absolute top-0 right-0 ma1 ma4-ns fr f4 dim pointer"
-              title="close ${label}"
-              aria-hidden="true"
-              onclick=${() =>{ emit('layout:toggleMenuItem', name, 'panels')}} >
-      </i>
+        <i
+                class="fas fa-times absolute top-0 right-0 ma1 ma4-ns fr f4 dim pointer"
+                title="close ${label}"
+                aria-hidden="true"
+                onclick=${() => {
+                  emit('layout:toggleMenuItem', name, 'panels')
+                }} >
+        </i>
         ${content}
       </div>
     `
@@ -36,20 +36,35 @@ const modal = (content, name, label, state, emit) => {
 module.exports = mainView
 function mainView (state, emit) {
   const settings = state.layout.settings
-  const floating = (content, {name, label, type = 'panels', flex='1'} ={}) => {
-    let showPanel =  state.layout[type][name] && !state.layout.collapsed
-    if(name === 'chat' && state.layout.forceChatOpen) showPanel = true // open chat even if menu is collapsed
-    let hidden = showPanel ? 'max-height:1000px;' :  "max-height:0px;overflow:hidden;border:none;"
-       const panel = html`
-        <div class="${showPanel?'w-100 ':'pv0'} panel bg-dark-gray ba flex flex-column ${(settings.columnLayout || !showPanel)? 'ma0': 'mv2 ma2-ns shadow-2'}"
-         style="border:1px solid #555;pointer-events:all;flex:${settings.columnLayout?flex:'0'};${hidden};">
+  const floating = (
+    content,
+    { name, label, type = 'panels', flex = '1' } = {}
+  ) => {
+    let showPanel = state.layout[type][name] && !state.layout.collapsed
+    if (name === 'chat' && state.layout.forceChatOpen) showPanel = true
+    // open chat even if menu is collapsed
+    const hidden = showPanel
+      ? 'max-height:1000px;'
+      : 'max-height:0px;overflow:hidden;border:none;'
+    const panel = html`
+        <div class="${showPanel
+        ? 'w-100 '
+        : 'pv0'} panel bg-dark-gray ba flex flex-column ${settings.columnLayout ||
+        !showPanel
+        ? 'ma0'
+        : 'mv2 ma2-ns shadow-2'}"
+         style="border:1px solid #555;pointer-events:all;flex:${settings.columnLayout
+        ? flex
+        : '0'};${hidden};">
           <div class="flex justify-between pa1">
             <div class="ttu">   <!-- my title  -->  </div>
             <i
                   class="fas fa-times self-end dim pointer pa1 mid-gray"
                   title="close ${label}"
                   aria-hidden="true"
-                  onclick=${() =>{ emit('layout:toggleMenuItem', name, type)}} >
+                  onclick=${() => {
+                    emit('layout:toggleMenuItem', name, type)
+                  }} >
             </i>
           </div>
           <div class="pa3 pt0">
@@ -57,16 +72,16 @@ function mainView (state, emit) {
           </div>
         </div>
       `
-      // listen for panel resize events in order to calculate panel margin
-      onload(panel, () => {
-        const resizeObserver = new ResizeObserver(entries => {
-          for (let entry of entries) {
-             emit('render')
-          }
-        })
-        resizeObserver.observe(panel)
+    // listen for panel resize events in order to calculate panel margin
+    onload(panel, () => {
+      const resizeObserver = new ResizeObserver(entries => {
+        for (let entry of entries) {
+          emit('render')
+        }
       })
-      return panel
+      resizeObserver.observe(panel)
+    })
+    return panel
   }
 
   if (!state.user.loggedIn) {
@@ -76,7 +91,7 @@ function mainView (state, emit) {
     </div>
     `
   } else {
-    if(state.user.callEnded === true) {
+    if (state.user.callEnded === true) {
       return html`<div class="pa5 f-headline">bye .... :] </div>`
     } else {
       return html`<div class="w-100 h-100" style="color:${state.style.colors.text1}">
@@ -84,29 +99,28 @@ function mainView (state, emit) {
           <div class="w-100 h-100 top-0 left-0 fixed flex flex-row-reverse-ns flex-column-reverse" style="pointer-events:none">
             ${menu(state, emit)}
             <div class="flex flex-column flex-wrap-reverse-ns flex-wrap justify-end w-100 w6-ns">
-              ${['a', 'b', 'c', 'd'].map((switcher) => floating(
-                state.cache(Switcher, `switcher-${switcher}`).render(switcher, state), {name: switcher, label:`switcher ${switcher}`, type:'switchers'})
-              )}
-              ${floating(settingsPanel(state, emit), {name:'settings', label:'settings'})}
-              ${floating(state.cache(Audio, 'audio-el').render(state.multiPeer.streams), {name:'audio', label:'volume controls'})}
-              ${floating(state.cache(Chat, 'chat-el').render(state), {name:'chat', label:'chat', flex:'1'})}
-              ${floating(peersList(state.multiPeer), {name: 'users', label: 'participants currently in room'})}
+              ${['a', 'b', 'c', 'd']
+                .map(switcher => floating(state.cache(Switcher, `switcher-${switcher}`).render(switcher, state), { name: switcher, label: `switcher ${switcher}`, type: 'switchers' }))}
+              ${floating(settingsPanel(state, emit), { name: 'settings', label: 'settings' })}
+              ${floating(state.cache(Audio, 'audio-el').render(state.multiPeer.streams), { name: 'audio', label: 'volume controls' })}
+              ${floating(state.cache(Chat, 'chat-el').render(state), { name: 'chat', label: 'chat', flex: '1' })}
+              ${floating(peersList(state.multiPeer), { name: 'users', label: 'participants currently in room' })}
               <div></div>
             </div>
 
             ${modal(state.cache(AddMedia, 'add-audio').render({
-              saveText: "add media stream",
-              onCancel: () => { emit('layout:toggleMenuItem', 'addMedia', 'panels')},
-              showLabel: true,
-              state: state,
-              onSave: ({ stream, mediaObj }) => {
-                emit('layout:toggleMenuItem', 'addMedia', 'panels')
-                emit('user:addStream', stream, mediaObj.label)
-              }
-            }), 'addMedia', 'add media', state, emit)}
-
+                saveText: 'add media stream',
+                onCancel: () => { emit('layout:toggleMenuItem', 'addMedia', 'panels') },
+                showLabel: true,
+                state: state,
+                onSave: ({ stream, mediaObj }) => {
+                  emit('layout:toggleMenuItem', 'addMedia', 'panels')
+                  emit('user:addStream', stream, mediaObj.label)
+                }
+              }), 'addMedia', 'add media', state, emit
+            )}
           </div>
       </div>`
     }
-   }
+  }
 }
--- app/views/switcher.js
+++ app/views/switcher.js
@@ -12,21 +12,18 @@ module.exports = class Switcher extends Component {
 
     // explicitly listen to update because not always called
     state.multiPeer.on('update', () => {
-      console.log('MULTIPEER UPDATE RECEIVED', Date.now())
       this.update(state.multiPeer.streams)
     })
-
     this.emit = emit
-    //console.log('audio init', streams, emit)
   }
 
-  update(streams) {
+  update (streams) {
     return true
   }
 
-  setWindowVideo() {
-    if(this.windowVideo) {
-      if(this.stream) {
+  setWindowVideo () {
+    if (this.windowVideo) {
+      if (this.stream) {
         this.windowVideo.srcObject = this.stream.stream
       } else {
         this.windowVideo.srcObject = null
@@ -34,72 +31,84 @@ module.exports = class Switcher extends Component {
     }
   }
 
-  clear() {
-    console.log('setting switcher', this.name, this.state)
+  clear () {
     this.state.layout.settings.switchers[this.name] = null
     this.rerender()
     this.emit('render')
   }
 
-  openWindow(title) {
-    const settings = {width: 1280, height: 720}
-    var windowSettings = `popup=yes,menubar=no,titlebar=no,location=no,scrollbars=no,status=no,toolbar=no,location=no,chrome=yes,width=${settings.width},height=${settings.height}`;
-   var win = window.open('', title, windowSettings)
-   // specifying a name for the second setting returns a reference to the same window, could be useful for setting output
-  // var win = window.open('', 'hey', windowSettings)
+  openWindow (title) {
+    const settings = { width: 1280, height: 720 }
+    var windowSettings = `popup=yes,menubar=no,titlebar=no,location=no,scrollbars=no,status=no,toolbar=no,location=no,chrome=yes,width=${settings.width},height=${settings.height}`
+    var win = window.open('', title, windowSettings)
+    // specifying a name for the second setting returns a reference to the same window, could be useful for setting output
+    // var win = window.open('', 'hey', windowSettings)
     win.document.body.style.background = 'black'
     win.document.title = title
     win.document.body.innerHTML = ''
-      var vid = win.document.createElement('video')
-      vid.autoplay = "autoplay"
-      vid.loop = "loop"
-      vid.muted = "muted"
-      vid.style.width = "100%"
-      vid.style.height = "100%"
-      vid.style.objectFit = "contain"
-      vid.style.opacity = this.opacity/100
-      win.document.body.style.padding = "0px"
-      win.document.body.style.margin = "0px"
-      win.document.body.appendChild(vid)
-      this.window = win
-      this.windowVideo = vid
-      this.setWindowVideo()
+    var vid = win.document.createElement('video')
+    vid.autoplay = 'autoplay'
+    vid.loop = 'loop'
+    vid.muted = 'muted'
+    vid.style.width = '100%'
+    vid.style.height = '100%'
+    vid.style.objectFit = 'contain'
+    vid.style.opacity = this.opacity / 100
+    win.document.body.style.padding = '0px'
+    win.document.body.style.margin = '0px'
+    win.document.body.appendChild(vid)
+    this.window = win
+    this.windowVideo = vid
+    this.setWindowVideo()
   }
 
-  setOpacity(opacity) {
+  setOpacity (opacity) {
     this.opacity = opacity
-    if(this.windowVideo) this.windowVideo.style.opacity = this.opacity/100
+    if (this.windowVideo) this.windowVideo.style.opacity = this.opacity / 100
     this.rerender()
   }
 
-  createElement(name, state) {
+  createElement (name, state) {
     this.name = name
     this.label = `switcher ${name}`.toUpperCase()
     this.state = state
     const stream = state.layout.settings.switchers[name]
-    if(stream !== this.stream) {
+    if (stream !== this.stream) {
       this.stream = stream
       this.setWindowVideo()
     }
     return html`<div class="relative">
       <div class='relative bg-black' style='height:140px'>
-        ${state.cache(Video, `video-${name}`).render(stream?stream.stream:null, {objectFit: 'contain', background:'black', opacity: this.opacity/100, position: 'absolute', width:'calc(100%-20px)'})}
-        <div class="absolute bottom-0 pa2">${stream ? `${stream.peer.nickname} ${stream.name.length > 0 ? `- ${stream.name}`: ''}`: ''}</div>
+        ${state
+      .cache(Video, `video-${name}`)
+      .render(stream ? stream.stream : null, {
+        objectFit: 'contain',
+        background: 'black',
+        opacity: this.opacity / 100,
+        position: 'absolute',
+        width: 'calc(100%-20px)'
+      })}
+        <div class="absolute bottom-0 pa2">${stream
+      ? `${stream.peer.nickname} ${stream.name.length > 0
+        ? `- ${stream.name}`
+        : ''}`
+      : ''}</div>
         <div class='slider-container'>
-          <input type="range" orient="vertical" value=${this.opacity} min="1" max="100" class="slider" style='right:20px;top:0px;width:140px' oninput=${(e)=>{this.setOpacity(e.target.value)}}>
+          <input type="range" orient="vertical" value=${this.opacity} min="1" max="100" class="slider" style='right:20px;top:0px;width:140px' oninput=${e => {
+            this.setOpacity(e.target.value)
+          }}>
         </div>
       </div>
       ${this.label}
       <span class="mh2"> | </span>
       <i
-        onclick=${()=> this.openWindow(`Live Lab - ${this.label}`)}
+        onclick=${() => this.openWindow(`Live Lab - ${this.label}`)}
         class="fas fa-external-link-alt dim pointer ma2" title="open video into it's own window">
       </i>
        <i
-         onclick=${()=> this.clear()}
+         onclick=${() => this.clear()}
          class="fas fa-times-circle dim pointer ma2" title="clear switcher">
        </i>
       </div>`
   }
-
 }
