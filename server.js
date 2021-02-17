88fb9501ef8eef98486375263a65cfdbb45e1e23
Tong <tongwu226@gmail.com>
2021-02-17 02:21:03 -0500

    set up local/production switch in server.js

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ var app = express()
 var browserify = require('browserify-middleware')
 var https = require('https')
 
-//require('dotenv').config();
+require('dotenv').config();
 
-var privateKey = fs.readFileSync(__dirname + '/certs/privkey.pem', 'utf8')
-var certificate = fs.readFileSync(__dirname + '/certs/fullchain.pem', 'utf8')
+//switch between localhost and website SSL credentials using environment variables 
+var privateKey = process.env.NODE_ENV === 'localrun' ?
+  fs.readFileSync(__dirname + '/certs/localhost-key.pem', 'utf8') :
+  fs.readFileSync(__dirname + '/certs/privkey.pem', 'utf8');
 
+var certificate = process.env.NODE_ENV === 'localrun' ?
+  fs.readFileSync(__dirname + '/certs/localhost.pem', 'utf8') :
+  fs.readFileSync(__dirname + '/certs/fullchain.pem', 'utf8');
 
 var credentials = {
   key: privateKey,
@@ -17,10 +22,10 @@ var credentials = {
 
 var httpsServer = https.createServer(credentials, app)
 
-var portNumber = 8000
+var portNumber = process.env.PORT;
 
-httpsServer.listen(portNumber, function(){
-  console.log("server available at port "+portNumber)
+httpsServer.listen(portNumber, function () {
+  console.log("server available at port " + portNumber)
 })
 
-app.use(express.static(__dirname +'/public'))
+app.use(express.static(__dirname + '/public'))
