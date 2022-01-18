07c4a6079c207787d40d5a8af569d743251f5900
ojack <ojackson@gmail.com>
2022-01-18 13:17:53 +0100

    add osc channel

--- app/views/browserglue.js
+++ app/views/browserglue.js
@@ -8,6 +8,8 @@ module.exports = class Browserglue extends Component {
     super(id)
     const bg = new browserglue.Client()
     this.isConnected = false
+    this.channelDialogIsShowing = false
+
     console.log('created browserglue', bg)
     bg.on('connect', event => {
       console.log("[connect]", event)
@@ -26,32 +28,66 @@ module.exports = class Browserglue extends Component {
     bg.on('subscribe-port', ({ path, port }) => console.log("[subscribe-port]", msg));
     bg.on('unsubscribe-port', ({ path, port }) => console.log("[unsubscribe-port]", msg));
     this.bg = bg
+
+    this.localChannels = []
+    this.remoteChannels = []
   }
 
   update () {
     return false
   }
 
-  async addChannel({name = 'test', port = '54321'}) {
+  async addChannel({name = '/test', port = '54321'}) {
     const thisChannel = await this.bg.addChannel(name, port)
 
     thisChannel.on('message', async blob => {
+      console.log('got message')
       const text = await blob.text();
       console.log("[/bar]", text);
       // how to parse to an object?
     });
+
+    console.log('created channel', thisChannel)
+    this.localChannels.push({ name, port, thisChannel, messages: ''})
+    this.channelDialogIsShowing = false
+    this.rerender()
+  }
+
+  showAddChannel() {
+    this.channelDialogIsShowing = true
+    this.rerender()
   }
 
   createElement (state) {
-    return html`<div>
+    let content = ''
+    if(this.channelDialogIsShowing) {
+      content = html`<div>
+        Add a channel
+        <span> Port </span>
+        <input placeholder="port"></input>
+        <span> Name </span>
+        <input placeholder="name"></input>
+        ${button({
+          text: 'add',
+          onClick: this.addChannel.bind(this),
+          classes: 'bg-dark-pink b fr mv2'
+        })}
+      </div>`
+    } else {
+      content =  html`
       ${this.isConnected ? html`<div>connected to browserglue at ${this.bg.url}
       ${button({
         text: 'add channel',
-        onClick: this.addChannel.bind(this),
+        onClick: this.showAddChannel.bind(this),
         classes: 'bg-dark-pink b fr mv2'
       })}
         </div>`
       : `browserglue not connected`}
+      <div class="br pt2"> Local </div>
+      ${this.localChannels.map((channel) => html`<div class="ba pa2">${channel.name} | ${channel.port}</div>`)}`
+    }
+    return html`<div>
+     ${content}
     </div>`
   }
 }
